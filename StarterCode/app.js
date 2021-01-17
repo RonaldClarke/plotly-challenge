9a563157171d6af336e6f11a1a7f3b50b8143179
Ronald Clarke <ronnie.clarke@utoronto.ca>
2021-01-17 13:03:23 -0500

    experimenting

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,3 +1,6 @@
 d3.json("samples.json").then((data) => {
     console.log(data.samples)
+    var example = data.samples.filter(s => s.id === "940");
+    console.log(example);
+    sortData = example.sort((a, b) => b.sample_values - )
 });
